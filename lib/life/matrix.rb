e31799474b68fecb4d56046687c1193add07fea6
Alexey Nikitin <xoma@undev.ru>
2013-06-08 21:36:58 +0400

    include matrix as a module

--- lib/life/matrix.rb
+++ lib/life/matrix.rb
@@ -2,7 +2,6 @@
 
 module Life
   module Matrix
-    extend self
 
     def walk(original_matrix)
       new_matrix = []
@@ -15,6 +14,12 @@ module Life
         new_matrix.push(row)
       end
       return new_matrix
+    end
+
+    def get_element(i, j, matrix)
+      if (i > 0) && (j > 0) && row = matrix[i]
+        row[j]
+      end
     end
   end
 end
--- lib/life/world.rb
+++ lib/life/world.rb
@@ -2,31 +2,28 @@
 
 module Life
   class World
+    include Life::Matrix
     attr_accessor :current
 
     def initialize(first_generation)
-      self.current = Life::Matrix.walk(first_generation) do |i, j, elem|
+      self.current = walk(first_generation) do |i, j, elem|
         init_state = elem.respond_to?(:to_i) ? elem.to_i > 0 : elem
         Cell.new(self, i, j, init_state)
       end
     end
 
     def next_generation!
-      self.current = Life::Matrix.walk(current) do |i, j, elem|
+      self.current = walk(current) do |i, j, elem|
         Cell.new(self, i, j, elem.calc_new_state)
       end
     end
 
     def to_a
-      Life::Matrix.walk(current){ |i, j, cell| cell.to_i }
+      walk(current){ |i, j, cell| cell.to_i }
     end
 
     def get(i, j)
-      unless (i < 0) || (j < 0)
-        if row = current[i]
-          row[j]
-        end
-      end
+      get_element(i, j, current)
     end
   end
 end
